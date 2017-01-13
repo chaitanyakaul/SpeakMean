946dfba10b3f6d0398677e555db5e168a3a1f6b1
Jose Annunziato <jannunzi@gmail.com>
2017-01-12 23:11:21 -0500

    added faces

--- public/views/activity/activity-list.controller.client.js
+++ public/views/activity/activity-list.controller.client.js
@@ -14,6 +14,9 @@
                 {username: {_id: '456', first: "Aviva", avatarImage: "", city: 'Italy'}, language: 'Italian', minutes: '10'},
                 {username: {_id: '567', first: "Brayo", avatarImage: "", city: 'Brasil'}, language: 'Potuguese', minutes: '20'},
                 {username: {_id: '678', first: "Alice", avatarImage: "", city: 'Poland'}, language: 'Polish', minutes: '30'},
+                {username: {_id: '789', first: "Bob", avatarImage: "", city: 'Russia'}, language: 'Russian', minutes: '40'},
+                {username: {_id: '789', first: "Bob", avatarImage: "", city: 'Russia'}, language: 'Russian', minutes: '40'},
+                {username: {_id: '789', first: "Bob", avatarImage: "", city: 'Russia'}, language: 'Russian', minutes: '40'},
                 {username: {_id: '789', first: "Bob", avatarImage: "", city: 'Russia'}, language: 'Russian', minutes: '40'}
             ];
         }
