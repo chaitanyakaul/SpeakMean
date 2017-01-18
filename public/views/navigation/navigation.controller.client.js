6cc86c11d7797b9f0e3ff91f187a9784b2c5f610
Jose Annunziato <jannunzi@gmail.com>
2017-01-17 22:50:10 -0500

    added back button

---
+++ public/views/navigation/navigation.controller.client.js
@@ -0,0 +1,28 @@
+(function () {
+    angular
+        .module("SpeakApp")
+        .controller('NavigationController', NavigationController);
+
+    function NavigationController($location) {
+        var vm = this;
+        vm.backButton = backButton;
+
+        console.log('rew');
+
+        function init() {
+        }
+        init();
+        
+        function backButton() {
+            var url = $location.url();
+            if (url === '/search') {
+                return 'activity';
+            } else if (url === "/search-results") {
+                return 'search';
+            } else if (url === '/contact') {
+                return 'search-results';
+            }
+            return null;
+        }
+    }
+})();
