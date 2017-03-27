b941c55f5d86d9681700739350e13aaf441696f2
Jose Annunziato <jannunzi@gmail.com>
2017-03-27 00:07:30 -0400

    added rating categories

--- app/models/session/session.schema.server.js
+++ app/models/session/session.schema.server.js
@@ -5,7 +5,20 @@ module.exports = function () {
         started: {type: Date, default: Date.now},
         ended:   {type: Date, default: Date.now},
         caller:  {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
-        called:  {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
+        called:  {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
+        learnerRating: { // learner rates the following attributes
+            coachRating: Number,
+            moduleRating: Number,
+            technicalQuality: Number,
+            favorite: Boolean
+        },
+        coachRating: { // coach rates the following attributes
+            learnerEffort: Number,
+            learnerSkill: Number,
+            learnerSpeaking: Number,
+            learnerListening: Number,
+            technicalQuality: Number
+        }
     }, {collection: 'session'});
 
     return SessionSchema;
