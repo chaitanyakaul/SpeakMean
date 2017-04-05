9583d1c59cbd8e9df8cc4de4067409a7b10f1876
Jose Annunziato <jannunzi@gmail.com>
2017-04-05 14:33:06 -0400

    getting started with quiz

---
+++ public/views/dictionary/controllers/dictionary-quiz.controller.client.js
@@ -0,0 +1,21 @@
+(function () {
+    angular
+        .module('SpeakApp')
+        .controller('DictionaryQuizController', DictionaryQuizController);
+
+    function DictionaryQuizController($routeParams, DictionaryService, $location) {
+        var vm = this;
+        vm.dictionaryId = $routeParams.dictionaryId;
+
+        function init() {
+            DictionaryService
+                .findDictionaryById(vm.dictionaryId)
+                .then(function (dictionary) {
+                    vm.dictionary = dictionary;
+                }, function (error) {
+                    console.log(error)
+                });
+        }
+        init();
+    }
+})();
