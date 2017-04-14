fa059163c88dbd9ae7592a20980549a1120ed2e8
Jose Annunziato <jannunzi@gmail.com>
2017-04-14 19:29:57 -0400

    trying to get websockets to work on heroku

--- public/views/navigation/navigation.controller.client.js
+++ public/views/navigation/navigation.controller.client.js
@@ -10,7 +10,7 @@
         vm.logout = logout;
 
         // SocketService.socket = io('http://localhost:3000');
-        SocketService.socket = io('https://speak-mean.herokuapp.com');
+        SocketService.socket = io('http://speak-mean.herokuapp.com');
         SocketService.socket.on('spk-broadcast', function(message){
             if(message.coach._id == $rootScope.user._id) {
                 var accept = confirm(message.coach.username + ', you have a call from ' + message.learner.username + '. Do you wish to accept?');
